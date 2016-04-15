cdae9cf647206dfa9d9953dc93a4da97561afa3d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-04-15 19:42:17 +0200

    Enable GZIP compression

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 
 var routes = require('./routes/index');
 var users = require('./routes/user');
@@ -20,6 +21,8 @@ app.locals.ENV_DEVELOPMENT = env == 'development';
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(compression());
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
